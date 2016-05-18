1c990d6ca47784942c0a18665dd2545cba5236d7
Jordan <jwells.me@gmail.com>
2016-05-18 11:04:43 -0400

    Tile out of bounds exception bugfix

--- src/game/physics/AABoundingRect.java
+++ src/game/physics/AABoundingRect.java
@@ -38,7 +38,15 @@ public class AABoundingRect extends BoundingShape {
         //we go from the left of the rect towards to right of the rect, making sure we round upwards to a multiple of 32 or we might miss a few tiles
         for(int i = (int) x; i <= x+width+(16-width%16); i+=16){
             for(int j = (int) y; j <= y+height+(16-height%16); j+=16){
-                occupiedTiles.add(tiles[i/16][j/16]);
+                i = i/16;
+                j = j/16;
+                if (i >= 100) {
+                    i = 100;
+                }
+                if (j >= 100) {
+                    j = 100;
+                }
+                occupiedTiles.add(tiles[i][j]);
             }
         }
         return occupiedTiles;
