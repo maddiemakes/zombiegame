9bc92893ee9ab25860c28d6b2001526db2b0f6be
Samuel Murray <smurra28@uncc.edu>
2019-07-13 20:47:58 -0500

    Add Mouse Look (rough)

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,9 +4,16 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 const SPEED = 125
+const SENSITIVITY = 10
 var motion = Vector2()
+var mouse_pos
 onready var sprite = get_node("sprite")
+onready var map = get_node("TileMap")
 var anim = "down"
+
+func is_deadzone():
+	return abs(mouse_pos.x - position.x) < SENSITIVITY or abs(mouse_pos.y - position.y) < SENSITIVITY
+	pass
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -16,23 +23,28 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		motion.y = SPEED
-		anim = "down"	
 	elif Input.is_action_pressed("ui_up"):
 		motion.y = -SPEED
-		anim = "up"
 	else:
 		motion.y = 0
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
-		anim = "right"
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
-		anim = "left"	
 	else:
 		motion.x = 0
 	if motion.x == 0 and motion.y == 0:
 		sprite.playing = false
 		sprite.set_frame(0)
+	mouse_pos = get_global_mouse_position()
+	if mouse_pos.y > position.y:
+		anim = "down"
+	elif mouse_pos.y < position.y:
+		anim = "up"
+	if mouse_pos.x > position.x and !is_deadzone():
+		anim= "right"
+	elif mouse_pos.x < position.x and !is_deadzone():
+		anim =  "left"
 	sprite.play(anim)
 	move_and_slide(motion)	
 	pass
